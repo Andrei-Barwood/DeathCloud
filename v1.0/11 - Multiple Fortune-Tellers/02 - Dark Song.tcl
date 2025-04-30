1c43890871e771e3cbc518c6fb792c5ef5054deb
Andrei <andresbarbudo@icloud.com>
2025-04-29 20:57:56 -0400

    v1.11-1 - Exponenciación - 1.2

---
+++ v1.0/11 - Multiple Fortune-Tellers/02 - Dark Song.tcl
@@ -0,0 +1,19 @@
+
+proc addkar {a b len result_list} {
+    set sum_list {}
+    set carry 0
+    set i 0
+    set j 0
+
+    while {$i < [llength $a] || $j < [llength $b] || $carry} {
+        set digit_a [expr {$i < [llength $a] ? [lindex $a $i] : 0}]
+        set digit_b [expr {$j < [llength $b] ? [lindex $b $j] : 0}]
+        set sum [expr {$digit_a + $digit_b + $carry}]
+        lappend sum_list [expr {$sum % 10}]
+        set carry [expr {$sum / 10}]
+        incr i
+        incr j
+    }
+    upvar 0 $result_list result
+    set result $sum_list
+}
